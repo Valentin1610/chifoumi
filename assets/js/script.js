875a5647f8654d0ebedecbc4282422dac41a5326
Valentin1610 <val.sucaud@gmail.com>
2023-07-10 14:12:39 +0200

    V1.3 Chimoufi

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ let titleStart = document .getElementById('titlestart')
 let titleSecond = document.getElementById('titlesecond')
 let soundBattle = document.getElementById('sound')
 let battleUser = document.getElementById('battle')
+let resetGameBtn = document.getElementById('buttonreset')
+let score = document.getElementById('score')
+let percentage = document.getElementById('wins')
 
 let wins = 0; let loses = 0;
 
@@ -31,8 +34,6 @@ function hiddenButton () {
 function randomComputer (userChoice){
     const computerChoiceGame = choices[Math.floor(Math.random() *choices.length)];
 
-    console.log(computerChoiceGame);
-
     switch(userChoice + '_' + computerChoiceGame){
         case 'Feuille_Ciseaux':
         case 'Pierre_Feuille':
@@ -41,27 +42,39 @@ function randomComputer (userChoice){
             gameScore.innerHTML = "PERDU"
 
             break;
-        case 'Feuille_Pierre':
+            case 'Feuille_Pierre':
         case 'Pierre_Ciseaux':
         case 'Ciseaux_Feuille':
             wins += 1;
             gameScore.innerHTML ="GAGNÉ"
             break;
-        case 'Feuille_Feuille':
-        case 'Pierre_Pierre':
+            case 'Feuille_Feuille':
+                case 'Pierre_Pierre':
         case 'Ciseaux_Ciseaux':
             gameScore.innerHTML = 'ÉGALITÉ'
             break; 
-    }
+        }
     choiceComputer.innerHTML = "Choix de l'ordinateur : " + computerChoiceGame
+    percentage.classList.remove('d-none')
+    resetGameBtn.classList.remove('d-none')
     battleScore.classList.remove('d-none')
     battleScoreComputer.classList.remove('d-none')
     battleScore.innerHTML = "Joueur : " + wins
     battleScoreComputer.innerHTML = "Ordinateur : " + loses
-
 }
 
 buttonStart.addEventListener('click', hiddenButton)
 grolem.addEventListener('click', () => randomComputer ("Pierre") )
 mystherbe.addEventListener('click', () => randomComputer ("Feuille"))
 insecateur.addEventListener('click', () => randomComputer ("Ciseaux"))
+
+const restartButton = document.getElementById('reset')
+restartButton.addEventListener('click', () => {
+    wins = 0;
+    loses = 0;
+    score.forEach(cell => {
+        cell.classList.remove("pierre", "feuille", "ciseaux") 
+        battleScore.innerHTML = "Joueur : 0"
+        battleScoreComputer.innerHTML = "Ordinateur : 0"
+    });
+})
